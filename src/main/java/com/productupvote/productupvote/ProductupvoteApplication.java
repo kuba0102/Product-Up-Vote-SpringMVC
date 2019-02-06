9fab9c4a79192e08c516ba65bdeb07f685578da9
kuba0102 <kubachruslicki@op.pl>
2019-02-06 16:34:51 +0000

    Initial Commit

---
+++ src/main/java/com/productupvote/productupvote/ProductupvoteApplication.java
@@ -0,0 +1,14 @@
+package com.productupvote.productupvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductupvoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductupvoteApplication.class, args);
+    }
+
+}
+
